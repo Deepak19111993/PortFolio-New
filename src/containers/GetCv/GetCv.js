b72d3c3367fa33bbdb92d5d831e1a2913d6c6086
Deepak Kushwaha <deepak.kushwaha@creolestudios.com>
2022-01-27 13:26:21 +0530

    first commit

--- src/containers/GetCv/GetCv.js
+++ src/containers/GetCv/GetCv.js
@@ -2,7 +2,6 @@ import React from "react";
 import { CustomButton } from "../../assets/styles/Global.styles";
 import { GetCvWrapper } from "./GetCv.styles";
 import GetCvBlock from "./GetCvBlock";
-import cv from "../../assets/images/cv.png";
 
 const GetCv = () => {
   return (
